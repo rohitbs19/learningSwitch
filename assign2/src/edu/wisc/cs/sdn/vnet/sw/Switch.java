a0e29bb8045010d3bfd939407f9ce7f23e7fc6f9
TurbulentCupcake <muraliadithya315@gmail.com>
2018-02-25 20:36:28 -0600

    implemented the timing part too; untested

--- assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -15,6 +15,7 @@ public class Switch extends Device
 
 	static private Map<MACAddress, Iface> forwardingTable = new HashMap<>();
 	private Map<String, Iface> interfaces;
+	static private Map<MACAddress, Integer> timings = new HashMap();
 
 
 	/**
@@ -24,6 +25,23 @@ public class Switch extends Device
 	public Switch(String host, DumpFile logfile)
 	{
 		super(host,logfile);
+	}
+
+	// this function is used to checktime, if a certain time
+	// has elapsed 15 seconds, then it will return true
+	// else it will return false
+	private boolean checkTime(MACAddress m) {
+
+		if(System.currentTimeMillis() - timings[m] >= 15*1000) {
+			return true;
+		}
+		return false;
+
+	}
+
+	private void setTime(MACAddress m) {
+
+		timings.put(m, System.currentTimeMillis());
 	}
 
 	/**
@@ -39,9 +57,24 @@ public class Switch extends Device
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
 		
-
+		// if the source mac address does not exist, then place that in
+		// the hashmap and update the timing
 		if(!forwardingTable.containsKey(etherPacket.getSourceMACAddres())) {
-			forwardingTable.put(etherPacket.getSourceMACAddres(),inIface);
+			forwardingTable.put(etherPacket.getSourceMACAddres(),inIface); // update table
+			setTime(etherPacket.getSourceMACAddres()); // set the timing
+		} else {
+			// if it does exist, then update the timing for this mac address in the timings
+			setTime(etherPacket.getSourceMACAddres());
+
+		}
+
+		// check if the other mac addresses, have been accesses in the 
+		// 15 seconds since the last time that packet arrived
+		for(MACAddress m : timings) {
+			if(checkTime(m)) {
+				forwardingTable.remove(m);
+				timings.remove(m);
+			}
 		}
 
 
@@ -59,7 +92,8 @@ public class Switch extends Device
 					// accepted our packet, which means we can
 					// add it to our table.
 					if(sendPacket(etherPacket, interfaces[name]) {
-						forwardingTable.put(etherPacket.getDestinationMACAddress(),interfaces[name]);
+						forwardingTable.put(etherPacket.getDestinationMACAddress(),interfaces[name]); // update table
+						//setTime(etherPacket.getDestinationMACAddress()); // set timing
 					}
 
 
