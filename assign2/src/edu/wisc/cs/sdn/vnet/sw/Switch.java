3febcf83df928a5a9cff3f998caf1a439f90a1a2
TurbulentCupcake <muraliadithya315@gmail.com>
2018-02-25 19:47:18 -0600

    prelimiary skeleton code; untested

--- assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -4,12 +4,19 @@ import net.floodlightcontroller.packet.Ethernet;
 import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
+import java.util.HashMap;
 
 /**
  * @author Aaron Gember-Jacobson
  */
 public class Switch extends Device
 {	
+
+
+	static private Map<MACAddress, Iface> forwardingTable = new HashMap<>();
+	private Map<String, Iface> interfaces;
+
+
 	/**
 	 * Creates a router for a specific host.
 	 * @param host hostname for the router
@@ -32,6 +39,42 @@ public class Switch extends Device
 		/********************************************************************/
 		/* TODO: Handle packets                                             */
 		
+
+		if(!forwardingTable.containsKey(etherPacket.getSourceMACAddres())) {
+			forwardingTable.put(etherPacket.getSourceMACAddres(),inIface);
+		}
+
+
+		// check if the ethernet destination host exists within the hashmap 
+		if(forwardingTable[etherPacket.getDestinationMACAddress()] == null) {
+
+			// if it does not exist in the table, broadcast first 
+				interfaces = getInterfaces();
+				
+				// iterate through the set of keys
+				for(String name : interfaces.keySet()) {
+
+					// if sendPacket returns true, this means
+					// that the destination host has
+					// accepted our packet, which means we can
+					// add it to our table.
+					if(sendPacket(etherPacket, interfaces[name]) {
+						forwardingTable.put(etherPacket.getDestinationMACAddress(),interfaces[name]);
+					}
+
+
+				}
+
+
+		} else {
+			// in this case, we assume that the destination MAC address 
+			// has an interface associated with it. 
+
+			sendPacket(etherPacket, forwardingTable[etherPacket.getDestinationMACAddress()]);
+
+		}
+		
+
 		/********************************************************************/
 	}
 }
