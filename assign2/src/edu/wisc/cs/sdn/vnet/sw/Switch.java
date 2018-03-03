ff15498e6d1e9cb468407dce0b09cb8c390ba893
rohitbs19 <battula2@wisc.edu>
2018-03-02 16:20:04 -0800

    working not completely tested switch

--- assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
+++ assign2/src/edu/wisc/cs/sdn/vnet/sw/Switch.java
@@ -7,6 +7,8 @@ import edu.wisc.cs.sdn.vnet.Device;
 import edu.wisc.cs.sdn.vnet.DumpFile;
 import edu.wisc.cs.sdn.vnet.Iface;
 import net.floodlightcontroller.packet.MACAddress;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import java.util.Set;
 import java.util.*;
 /**
@@ -67,38 +69,19 @@ public class Switch extends Device
 		 * 4-2: is mapped to (interface).
 		 * 5: then in that case we flood to all the interfaces connected to the switch
 		 * 6: we flood by sending the packet to all the other interfaces.
-		 * 7: clarity: how do we know if the right interface took the packet ?
-		 * 7-2: clarity: does the interface send a ack back to the switch
-		 * 8: assumption: that the switch sends an ack and we know the right interface.
-		 * 9: we then update the mac mapping to include the new mapping of the destination address
-		 * 9-2: to the Mac address.
-		 * 10: Then we use the send packet method to send the packet to the destination mac address
-		 * 11: and specify the interface we found.
 		 * */
 		// extracts the destination mac address
-		////////////////////////////////////////// INIT VARS ////////////////////////////////////////////////////
 		MACAddress destAddr = etherPacket.getDestinationMAC();
 		// extracts the source mac address
 		MACAddress srcAddr = etherPacket.getSourceMAC();
 		//map containing all the mac addressed mapped to key that is its name
 		Map<String,Iface> interfaces = getInterfaces();
-        Set<String> temp2 = interfaces.keySet();
-        //itr for all the keys inside the interfaces map
-        Iterator<String> itr_key_name_2 = temp2.iterator();
-        String next_1_2 = null;
-        if(itr_key_name_2.hasNext()) {
-            next_1_2 = itr_key_name_2.next();
-        }
-        System.out.println("CHECK: THE NAMES OF ALL THE INTERFACES PRESENT");
-        while (itr_key_name_2.hasNext()) {
-            sendPacket(etherPacket, interfaces.get(next_1_2));
-            //if ack is true found destination interface.
-            System.out.println(interfaces.get(next_1_2).getName());
-            next_1_2 = itr_key_name_2.next();
-        }
-		////////////////////////////////////////// END //////////////////////////////////////////////////////////
 
-		///////////////////////////////////////// TRIVIAL CHECKING CONDS ///////////////////////////////////////
+
+		for (String name : interfaces.keySet()) {
+			System.out.println("names: " + interfaces.get(name).getName());
+		}
+
 		if (destAddr == null) {
 			System.out.println("Error: invalid destination mac address & address is null");
             System.exit(1);
@@ -108,97 +91,49 @@ public class Switch extends Device
 			System.out.println("Error: invalid src mac address & address is null");
             System.exit(1);
 		}
-		////////////////////////////////////////////////////////////////////////////////////////////////////////
 		/*code for removing entries based on timeouts*/
-		////////////////////////////////////////// TIMEOUTS DELETION ENTRY PART ////////////////////////////////
-		//gets the set of keys of the hash map
-		Set<MACAddress> macKeySet= hmap.keySet();
-		//System.out.println("check: null: macKeySet --> " + macKeySet);
-		//itr for the setkeys to iterate through all the keys
-		Iterator<MACAddress> itrMac_key = macKeySet.iterator();
-        MACAddress next = null;
-        if (itrMac_key.hasNext()) {
-            next = itrMac_key.next();
-        }
-
-        //System.out.println("check: null: itrMac_key --> " + itrMac_key);
-
-		// checks if the next elem is present in the set
-		while (itrMac_key.hasNext()) {
-			// checking condition for time elapsed
-
-			if((System.currentTimeMillis() - hmap.get(next).getCurrTime()) > 15*1000){
-				//removes if the difference of time is more than 15000 ms
-				hmap.remove(next);
+		//print the hashmap
+		for (MACAddress name : hmap.keySet()) {
+			System.out.println(hmap.get(name).GetIface().getName());
+		}
+  		for (String name : interfaces.keySet()) {
+			if (hmap.get(name)!=null && System.currentTimeMillis() - hmap.get(name).getCurrTime()> (15000)) {
+				hmap.remove(name);
 			}
-            next = itrMac_key.next();
 		}
-		/*end*/
-		/////////////////////////////////////// END ///////////////////////////////////////////////////////////
-
-		/////////////////////////////////// LOOKUP FOR SRC AND DEST ///////////////////////////////////////////
+		
 		//checks if the destination mac address is inside the has map
-		boolean exists = false;
 		// temp to hold the looked up dest addr
-		Wrapper look_dest = hmap.get(destAddr);
-		if (look_dest == null) {
-			exists = false;
-		}else{
-			exists = true;
-		}
-
 		//temp for holding looked up src in hash map
-		Wrapper look_src = hmap.get(srcAddr);
-
-		if (look_src == null) {
+		if (hmap.get(srcAddr) == null) {
 			Wrapper new_src = new Wrapper(System.currentTimeMillis(), inIface);
 			hmap.put(srcAddr, new_src);
 		}else{
-			look_src.setCurrTime(System.currentTimeMillis());
+			hmap.get(srcAddr).setCurrTime(System.currentTimeMillis());
 		}
-		/////////////////////////////////// END //////////////////////////////////////////////////////////////
-
+	        System.out.println("////////////////////////////////////////////////////////////////////////////");	
+		for (MACAddress name : hmap.keySet()) {
+			System.out.println(hmap.get(name).GetIface().getName());
+		}
+		System.out.println("////////////////////////////////////////////////////////////////////////////");
 		// if destination addr is inside the hash map send packet through the corresponding interface
-		///////////////////////////////// SEND PACKET & BROADCAST IF !exists ////////////////////////////////
-		if (exists) {
-			sendPacket(etherPacket, look_dest.GetIface());
-		}else{
+		
+		
+		if (hmap.get(destAddr)!=null && hmap.get(destAddr).GetIface()!=inIface) {
+			
+			sendPacket(etherPacket, hmap.get(destAddr).GetIface());
+		}else if(hmap.get(destAddr)==null ){
 			//broadcast/flood
-			boolean ack = false;
-			Iface foundDestInterface= null;
-			Set<String> temp = interfaces.keySet();
-			//itr for all the keys inside the interfaces map
-			Iterator<String> itr_key_name = temp.iterator();
-            String next_1 = null;
-			if(itr_key_name.hasNext()) {
-                 next_1 = itr_key_name.next();
-            }
-            while (itr_key_name.hasNext()) {
-
-			    ack = sendPacket(etherPacket, interfaces.get(next_1));
-				//if ack is true found destination interface.
-				if (ack) {
-					foundDestInterface = interfaces.get(next_1);
+			for (String name : interfaces.keySet()) {
+				if(!name.equals(inIface.getName())){
+					sendPacket(etherPacket, interfaces.get(name));
 				}
-                next_1 = itr_key_name.next();
+				System.out.println("searching for reply from interface.. ==> " + interfaces.get(name).getName());
 			}
-			//found the destination interface update the table
-			//if interface is found through the flood process
-			if (foundDestInterface != null) {
-				//creates an entry into hash map via the wrapper class
-				// with the current time and dest interface
-                System.out.println("and the one that is found!");
-                System.out.println(foundDestInterface.getName());
-
-				Wrapper found_des = new Wrapper(System.currentTimeMillis(), foundDestInterface);
-				hmap.put(destAddr, found_des);
-
-			}else{
-				//if no interface exists
-				System.out.println("Error: no interface took that");
-                System.exit(1);
-			}
-
+		}
+		else{
+			System.out.println("exit");
+			System.exit(1);
 		}
 		///////////////////////////// END ////////////////////////////////////////////////////////////////
 
